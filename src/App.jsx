9688ad0e7ee49ed5313bcfae83da0c754e4570b5
Anton Kryzhanivskyi <firechant2@gmail.com>
2025-04-18 23:20:53 +0300

    code refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -28,9 +28,11 @@ export const App = () => {
               data-cy="ClearButton"
               type="button"
               className="delete ml-3"
-              onClick={() => setSelectedGood('')}
+              onClick={() => {
+                setSelectedGood('');
+              }}
             >
-              Clear
+              x
             </button>
           </>
         ) : (
