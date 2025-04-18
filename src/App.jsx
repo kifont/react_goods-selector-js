6bf4f565732be52e717fe53ca87a4959dbc89244
Anton Kryzhanivskyi <firechant2@gmail.com>
2025-04-18 18:19:03 +0300

    Add code

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,9 @@ export const App = () => {
               type="button"
               className="delete ml-3"
               onClick={() => setSelectedGood('')}
-            />
+            >
+              Clear
+            </button>
           </>
         ) : (
           'No goods selected'
